bbabc712a58b316cc69b14dc2fe9c2e135c7b4b4
Stephen Durbin <futtara@gmail.com>
2014-04-06 16:41:50 -0600

    Require WebOb and mysql-connector-python

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='LGCData',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Montana Local Government Center Data API',
+      author='Stephen Durbin',
+      author_email='futtara@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['bottle'],
-#      install_requires=['Django>=1.3'],
+      install_requires=['WebOb', 'bottle', 'mysql-connector-python'],
      )
