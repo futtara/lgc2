b98d3a4199655ff4f2b1694c79e458d2eaad69d9
Stephen Durbin <futtara@gmail.com>
2014-04-06 17:08:01 -0600

    Replace mysql-connector-python with PyMySQL

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='LGCData',
       author='Stephen Durbin',
       author_email='futtara@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['WebOb', 'bottle', 'mysql-connector-python'],
+      install_requires=['WebOb', 'bottle', 'PyMySQL'],
      )
