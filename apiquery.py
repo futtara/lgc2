098578ff176093b73119fbec397d30a549e3692a
Stephen Durbin <futtara@gmail.com>
2015-05-11 01:11:24 -0600

    Limit data years to 1991- except for local government review

--- apiquery.py
+++ apiquery.py
@@ -37,6 +37,9 @@ def get_sql(type, name, year, fields):
     if len(year) > 0 and year.lower() == 'all':
         sql += "AND year>1990 "
 
+    if len(year) > 0 and year.lower() == 'all-from-1974':
+        sql += "AND year>=1974 "
+
     if len(year) > 0 and year.lower() != 'all':
         quoted_years = "','".join(year.split(','))
         sql += "AND year IN ('%s') " % (quoted_years)
